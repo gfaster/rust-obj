eb86ef3a4eaf67dfeb00194f5a45f5ca227d942b
Cubostar <cubostar@gmail.com>
2023-06-20 16:00:24 -0400

    Work on image processing

--- src/depth_classify/mod.rs
+++ src/depth_classify/mod.rs
@@ -1,1 +1,37 @@
+use std::fmt::Display;
 
+use image::{RgbaImage, ImageBuffer, GrayImage};
+use image::imageops::colorops::grayscale_alpha;
+
+#[derive(Debug)]
+enum Error {
+    MismatchedDimensions,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: {:?}", self)
+    }
+}
+
+impl std::error::Error for Error { }
+
+fn loss(img1: RgbaImage, img2: RgbaImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
+    if img1.dimensions() != img2.dimensions() {
+        return Err(Error::MismatchedDimensions.into());
+    }
+
+    let gray_img1 = grayscale_alpha(&img1);
+    let gray_img2 = grayscale_alpha(&img2);
+    let (width, height) = img1.dimensions();
+
+    let mut ret = ImageBuffer::new(width, height);
+    for (x, y, pixel) in gray_img1.enumerate_pixels() {
+        ret.put_pixel(x, y, pixel)
+    }
+    todo!()
+}
+
+fn spectral_loss(img1: RgbaImage, img2: RgbaImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
+    todo!()
+}
