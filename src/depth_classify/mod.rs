2e7ae417006bd01c78e249ed01ded2ce284ce07b
Cubostar <cubostar@gmail.com>
2023-06-22 13:55:12 -0400

    Finish pixel_difference()

--- src/depth_classify/mod.rs
+++ src/depth_classify/mod.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
-use image::{RgbaImage, ImageBuffer, GrayImage};
-use image::imageops::colorops::grayscale_alpha;
+use image::{RgbaImage, GrayImage, Pixel};
+use image::imageops::colorops::grayscale;
 
 #[derive(Debug)]
 enum Error {
@@ -16,20 +16,21 @@ impl Display for Error {
 
 impl std::error::Error for Error { }
 
-fn loss(img1: RgbaImage, img2: RgbaImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
+fn pixel_difference(img1: RgbaImage, img2: RgbaImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
     if img1.dimensions() != img2.dimensions() {
         return Err(Error::MismatchedDimensions.into());
     }
 
-    let gray_img1 = grayscale_alpha(&img1);
-    let gray_img2 = grayscale_alpha(&img2);
+    let gray_img1 = grayscale(&img1);
+    let gray_img2 = grayscale(&img2);
     let (width, height) = img1.dimensions();
 
-    let mut ret = ImageBuffer::new(width, height);
+    let mut ret = GrayImage::new(width, height);
     for (x, y, pixel) in gray_img1.enumerate_pixels() {
-        ret.put_pixel(x, y, pixel)
+        ret.put_pixel(x, y, pixel.map(|v| v.abs_diff(gray_img2.get_pixel(x, y).channels()[0])))
     }
-    todo!()
+    
+    Ok(ret)
 }
 
 fn spectral_loss(img1: RgbaImage, img2: RgbaImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
