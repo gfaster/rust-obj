778669e64ca8aecf8c6b2b453fb6e55d85baf165
Cubostar <cubostar@gmail.com>
2023-06-27 12:07:11 -0400

    Fixing 2dft

--- src/depth_classify/mod.rs
+++ src/depth_classify/mod.rs
@@ -3,6 +3,8 @@ use std::fmt::Display;
 use image::{GrayImage, Pixel, DynamicImage, GenericImageView};
 use image::imageops::colorops::grayscale;
 
+use rustfft::{FftDirection, FftPlanner, num_complex::Complex};
+
 #[derive(Debug)]
 enum Error {
     MismatchedDimensions,
@@ -34,5 +36,17 @@ pub fn pixel_difference(img1: DynamicImage, img2: DynamicImage) -> Result<GrayIm
 }
 
 pub fn spectral_loss(img1: DynamicImage, img2: DynamicImage) -> Result<GrayImage, Box<dyn std::error::Error>> {
-    todo!()
+    if img1.dimensions() != img2.dimensions() {
+        return Err(Error::MismatchedDimensions.into());
+    }
+
+    let gray_img1 = grayscale(&img1);
+    let gray_img2 = grayscale(&img2);
+    let (width, height) = img1.dimensions();
+    
+    let mut planner = FftPlanner::new();
+    let fft_width = planner.plan_fft(width, FftDirection::Forward);
+    let mut scratch = vec![Complex::default(); fft_width.get_inplace_scratch_len()];
+    
+    todo!();
 }
